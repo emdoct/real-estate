7b572d397fde8908c479f4146c7256f8335b015c
emdoct <62918095+emdoct@users.noreply.github.com>
2020-07-24 16:43:19 +1000

    add slider, mobile menu, header adaptarion

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(function() {
+	$('.menu-toggle').on('click', function(){
+		$('.mobile-menu').toggleClass('hide-menu');
+		$('.burger').toggleClass('active');
+	});
+
+	
+
+	$('.search-tag2').on('click', function(){
+		$('.search-tag2').addClass('active-tag');
+		$('.search-tag1').removeClass('active-tag');
+		$('.tenants-form').removeClass('active-form');
+		$('.owners-form').addClass('active-form');
+	});
+
+	$('.search-tag1').on('click', function(){
+		$('.search-tag1').addClass('active-tag');
+		$('.search-tag2').removeClass('active-tag');
+		$('.owners-form').removeClass('active-form');
+		$('.tenants-form').addClass('active-form');
+
+	});
+
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+	});
+});
